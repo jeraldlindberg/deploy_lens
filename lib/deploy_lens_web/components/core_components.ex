d2d83a89b678691c64e001653b9845621026959d
Jerald Lindberg <jerald.d.lindberg@gmail.com>
2025-10-21 20:55:46 -0500

    feat: Add spinner and skeleton loaders
    This commit introduces new spinner and skeleton loader components to improve
    the user experience by providing visual feedback during loading states.
    
    The .gitignore file has also been updated to ignore the secrets.exs file.

--- lib/deploy_lens_web/components/core_components.ex
+++ lib/deploy_lens_web/components/core_components.ex
@@ -419,6 +419,47 @@ defmodule DeployLensWeb.CoreComponents do
     """
   end
 
+  @doc """
+  Renders a spinner icon.
+  """
+  attr :class, :string, default: "size-4"
+
+  def spinner(assigns) do
+    ~H"""
+    <svg
+      class={["animate-spin", @class]}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      />
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+    """
+  end
+
+  @doc """
+  Renders a skeleton placeholder.
+  """
+  attr :class, :string
+
+  def skeleton(assigns) do
+    ~H"""
+    <div class={["animate-pulse bg-base-200/60", @class]} />
+    """
+  end
+
   ## JS Commands
 
   def show(js \\ %JS{}, selector) do
